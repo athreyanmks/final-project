5f5a216bfcb0963637d8cb90b03cd49f1cd789d3
athreyanmks <30892530+athreyanmks@users.noreply.github.com>
2019-04-16 11:40:03 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,3 +1,43 @@
+function tiphat(x)
+{
+    console.log(x);
+
+    button = document.getElementById("button"+x);
+    if(button.value == 0)
+    {
+        button.value = 1;
+        button.style =  "background-color:blue";  
+    }
+
+    else if(button.value == 1)
+    {
+        button.value = 0;
+        button.style = "background-color:solid grey"
+    }
+
+        if (window.XMLHttpRequest) 
+        {
+            xmlhttp = new XMLHttpRequest();
+        } 
+        else 
+        {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("likec"+x).innerHTML = this.responseText;
+                //console.log('a');
+                            }
+        };
+        xmlhttp.open("GET","likeprocess.php?q="+button.value+"&id="+x,true);
+        xmlhttp.send();
+
+        document.getElementById("button"+x).innerHTML = "TipHat";
+
+
+}
+
 function uploadform()
 {
 	window.location = "uploadform.php";
@@ -29,6 +69,7 @@ function dynasearch(s)
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 document.getElementById("profsearch").innerHTML = this.responseText;
+                //console.log('a');
                             }
         };
         xmlhttp.open("GET","profsearch.php?q="+s,true);
@@ -64,3 +105,70 @@ function homefeed()
 
 	console.log("flag");
 }
+
+function commentsec(x)
+{
+    console.log("flag");
+        if (window.XMLHttpRequest) 
+        {
+            xmlhttp = new XMLHttpRequest();
+        } 
+        else 
+        {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("commentsec").innerHTML = this.responseText;
+                            }
+        };
+        xmlhttp.open("GET","commentsec.php?q="+x,true);
+        xmlhttp.send(); 
+
+    console.log("flag");
+}
+
+function commentlike(x)
+{
+    console.log(x);
+
+    button = document.getElementById("button"+x);
+    if(button.value == 0)
+    {
+        button.value = 1;
+        button.style =  "background-color:blue";  
+    }
+
+    else if(button.value == 1)
+    {
+        button.value = 0;
+        button.style = "background-color:solid grey"
+    }
+
+        if (window.XMLHttpRequest) 
+        {
+            xmlhttp = new XMLHttpRequest();
+        } 
+        else 
+        {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("commc"+x).innerHTML = this.responseText;
+                //console.log('a');
+                            }
+        };
+        xmlhttp.open("GET","commentlikeprocess.php?q="+button.value+"&id="+x,true);
+        xmlhttp.send();
+
+        document.getElementById("button"+x).innerHTML = "TipHat";
+}
+
+function replyreveal(x)
+{
+    document.getElementById('commentform'+x).style.display = "block";
+}
+
